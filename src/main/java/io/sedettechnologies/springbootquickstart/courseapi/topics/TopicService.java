f13bca8207d59d1db0fa026a6140aac98ebc7b19
Israel Clark <clarkisraelofficial@gmail.com>
2021-02-18 15:06:29 +0100

    added my project

---
+++ src/main/java/io/sedettechnologies/springbootquickstart/courseapi/topics/TopicService.java
@@ -0,0 +1,32 @@
+package io.sedettechnologies.springbootquickstart.courseapi.topics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("love","Love story", "Israel Clark is in love with Jemimah Moses"),
+			new Topic("hate","Hate story", "The hate you give little infants fucks everybody"),
+			new Topic("religion","Christ is king", "The whole whole must bow at the sound of the name of Jesus")
+
+			));
+	
+	public List getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		
+	}
+}
---
+++ src/test/java/io/sedettechnologies/springbootquickstart/courseapi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sedettechnologies.springbootquickstart.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
